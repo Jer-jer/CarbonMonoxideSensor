d9e36937b93a1f0c2cec69292135ed9f63bdb683
Allan Jericho <jerichoallan0@gmail.com>
2020-12-30 11:26:03 +0800

    Finished

--- database/sensorDatabase.sql
+++ database/sensorDatabase.sql
@@ -11,14 +11,3 @@ CREATE TABLE current_state(
 
     PRIMARY KEY(idNum)
 );
-
-CREATE TABLE overall_states(
-    id int(20) NOT NULL,
-    lastPPM float NOT NULL,
-    dateeSensed DATE NOT NULL UNIQUE,
-    levelss int NOT NULL,
-    stats varchar(50),
-
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES current_state(idNum)
-);
